d7b25a4aaebd591594cfe9fa8390e4deee941200
Errietta Kostala <errietta@fairfx.com>
2017-11-28 11:27:31 +0000

    Allow checking cert on a nonstandard port

--- cert-details.sh
+++ cert-details.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 #https://serverfault.com/questions/661978/displaying-a-remote-ssl-certificate-details-using-cli-tools
-echo | openssl s_client -showcerts -servername $1 -connect $1:443 2>/dev/null | openssl x509 -inform pem -noout -text
+PORT=${2:-443}
+echo | openssl s_client -showcerts -servername $1 -connect $1:$PORT 2>/dev/null | openssl x509 -inform pem -noout -text
